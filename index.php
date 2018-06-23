d7a7ebb2dad8fa0fa0927a03d6a4b73d3b778612
deependerrathore <deepenrathore28@gmail.com>
2018-06-23 17:22:06 +1000

    Setup for eua

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Event UnAcademy</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+    
+    <title>Event UnAcademy - Doing is Learning</title>
 </head>
 <body>
-    <h1>Event UnAcademy</h1>
-
-    <h2>Hello guys</h2>
+    
 </body>
 </html>
